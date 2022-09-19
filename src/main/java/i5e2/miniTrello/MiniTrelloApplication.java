87c184fb312f15dd794adb57bf2f23a2bf4d92a3
jangyunejae <yunejaega@gmail.com>
2022-09-19 20:24:31 +0900

    Spring Boot 프로젝트 생성 및 Dependencies추가
    <추가된 dependencies 목록>
    -Oracle Database Driver
    -Spring Web
    -Lombok
    -Spring Data JPA

---
+++ src/main/java/i5e2/miniTrello/MiniTrelloApplication.java
@@ -0,0 +1,13 @@
+package i5e2.miniTrello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTrelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTrelloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/i5e2/miniTrello/MiniTrelloApplicationTests.java
@@ -0,0 +1,13 @@
+package i5e2.miniTrello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTrelloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
